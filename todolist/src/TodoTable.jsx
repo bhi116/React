3eed88ed04c5ed27c2ba515b74e3a75e4fbe0707
bhi116 <bhi116@myy.haaga-helia.fi>
2025-03-11 20:12:05 +0200

    Filter + sort

--- todolist/src/TodoTable.jsx
+++ todolist/src/TodoTable.jsx
@@ -1,26 +1,27 @@
 
-export default function TodoTable({todos, removeTodo}) {
+// export default function TodoTable({todos, removeTodo}) {
  
-  return (
-  <>
-    <table>
-          <thead>
-            <tr>
-                <th>Description</th>
-                <th>Date</th>
-            </tr>
-            </thead>
-            <tbody>
-            {
-            todos.map((todo, index) => (
-            <tr key={index}>
-              <td>{todo.description}</td>
-              <td>{todo.date}</td>
-              <td><button onClick={() => removeTodo(index)}>Poista</button></td>
-            </tr>
-            ))}
-          </tbody>
-         </table>
-  </>
-  );
-}
+//  return (
+//  <>
+//    <table>
+//          <thead>
+//            <tr>
+//                <th>Description</th>
+//                <th>Date</th>
+//            </tr>
+//            </thead>
+//           <tbody>
+//            {
+//            todos.map((todo, index) => (
+//            <tr key={index}>
+//              <td>{todo.description}</td>
+//              <td>{todo.date}</td>
+//             <td>{todo.pr}</td>
+//              <td><button onClick={() => removeTodo(index)}>Poista</button></td>
+//            </tr>
+//            ))}
+//          </tbody>
+//         </table>
+//  </>
+// );
+// }
--- todolist/src/Todolist.jsx
+++ todolist/src/Todolist.jsx
@@ -1,37 +1,71 @@
-import { useState } from 'react'
-import TodoTable from "./TodoTable";
+import { AgGridReact } from "ag-grid-react";
+import { useRef, useState } from 'react'
+import { AllCommunityModule, ModuleRegistry } from 'ag-grid-community'; 
+
+// Register all Community features
+ModuleRegistry.registerModules([AllCommunityModule]);
 
 function TodoList() {
-     const [todo, setTodo] = useState({description: '', date: ''});
+     const [todo, setTodo] = useState({description: '', date: '', priority: ''});
       const [todos, setTodos] = useState([]);
+      const gridRef = useRef();
     
     
       const addTodo = (event) => {
         event.preventDefault();
+        if (!todo.description.trim() || !todo.date.trim() || !todo.priority.trim()) {
+          alert("All fields must be filled!");
+          return;
+        }
         console.log("Lisätään todo todos-taulukkoon");
         setTodos([...todos, todo]);
-        setTodo({ description: "", date: "" });
+        setTodo({ description: "", date: "", priority: "" });
       }
     
-      const removeTodo = (index) => {
-        setTodos(todos.filter((todo, i) => i !== index));
+      const deleteTodo = () => {
+        if (gridRef.current.getSelectedNodes().length > 0) {
+          setTodos(todos.filter((todo, index) => 
+            index != gridRef.current.getSelectedNodes()[0].id))
+        }
+        else {
+          alert('Select a row first!');
+        }
       };
+
+
+      const columns = [
+        {headerName: 'Description', field: 'description', sortable: true, filter: true},
+        {headerName: 'Date', field: 'date', sortable: true, filter: true},
+        {headerName: 'Priority', field: 'priority', sortable: true, filter: true,
+          cellStyle: params => params.value === "High" ? {color: 'red'} : {color: 'black'}
+        }
+      ]
     
       return (
         <>
         <h1>Add todo:</h1>
-         <form onSubmit={addTodo}>
+        <form onSubmit={addTodo} style={{ display: "flex", gap: "10px", alignItems: "center" }}>
           <label>Description:
-          <input type="text" value={todo.description} placeholder="Add todo" onChange={(event) => setTodo({...todo, description: event.target.value})}></input>
+          <input type="text" name="description" value={todo.description} placeholder="Add todo" onChange={(event) => setTodo({...todo, description: event.target.value})}></input>
           </label>
           <label>Date:
-          <input type="date" value={todo.date} onChange={(event) => setTodo({...todo, date: event.target.value})}></input>
+          <input type="date" name="date" value={todo.date} onChange={(event) => setTodo({...todo, date: event.target.value})}></input>
           </label>
-          <input type="submit" value="Add"></input>
+          <label>Priority:
+          <input type="text" name="priority" value={todo.priority} placeholder="Priority" onChange={(event) => setTodo({...todo, priority: event.target.value})}></input>
+          </label>
+          <button type="submit">Add</button>
+          <button type="button" onClick={deleteTodo}>Delete</button>
          </form>
-
-         <TodoTable todos={todos} removeTodo={removeTodo} />
-
+         <div style={{width: 700, height: 500}}>
+         <AgGridReact 
+          rowData={todos}
+          columnDefs={columns}
+          ref={gridRef}
+          rowSelection="single"
+          onGridReady={ params => gridRef.current = params.api }
+          />
+          </div> 
         </>
       )
     
